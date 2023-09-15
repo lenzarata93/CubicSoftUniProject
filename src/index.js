288759eb55426adc5692a640af71804c904db9aa
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-09-15 14:55:31 +0300

    add static asset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 const  app = express();
 const PORT =5000;
+
+// Express config
+app.use(express.static(path.resolve(__dirname,'public')));
+
 // Handlebars config
 app.engine('hbs',handlebars.engine({
     extname: 'hbs' 
