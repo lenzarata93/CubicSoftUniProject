5065f06989b3497eb28ff2ecdd29db4ef92d9b40
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-09-15 14:24:23 +0300

    Add Express to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const express = require('express');
+
+const  app = express();
+const PORT =5000;
+
+app.get('/',(req,res)=>{
+res.send('It work.')
+});
+
+app.listen(PORT,()=> console.log(`Server is running on port ${PORT}....`));
