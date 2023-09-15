dcc4ff93817be73098f93899e658f068ce150318
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-09-15 16:15:42 +0300

    small corection

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path =require('path')
+const path = require('path')
 
 function expressConfig(app){
     app.use(express.static(path.resolve(__dirname,'..','public')));
