1e6555bd3d0fe84a18a8ff1fd296e5692e5d7fed
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-09-19 13:31:17 +0300

    Set up mongoose

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 const express = require('express');
+
 const  app = express();
 const expressConfig=require('./config/expressConfig');
 const handlebarsConfig=require('./config/handlebarsConfig');
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const dbConnect = require('./config/dbConfig');
+
+
 
 const PORT =5000;
 
+dbConnect()
+.then(()=> console.log('DB Connected....'))
+.catch(err =>{
+    console.log('DB error:', err);
+});
 expressConfig(app);
 handlebarsConfig(app);
 
 app.use(homeController);
 app.use('/cubes', cubeController);
 app.get('*',(req,res)=>{
-res.redirect('/404')
+res.redirect('/404');
 });
 
 
