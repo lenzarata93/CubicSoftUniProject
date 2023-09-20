a0ae5fbd3f6857492406f6606e756e142c2c543d
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-09-20 15:16:29 +0300

    show cube accessories on attach page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create',(req,res)=>{
     console.log(cubeManager.getAll());
@@ -31,8 +32,10 @@ router.get('/:cubeId/details', async(req,res)=>{
     res.render('details', {cube});
     });
 
-    router.get('/:cubeId/attach-accessory',(req,res)=>{
-res.render('accessory/attach');
+    router.get('/:cubeId/attach-accessory',async (req,res)=>{
+        const cube =await cubeManager.getOne(req.params.cubeId).lean();
+        const accessories = await accessoryManager.getAll().lean();
+res.render('accessory/attach',{cube,accessories});
     });
 
 module.exports=router;
--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,3 +1,3 @@
 const Accessory = require('../models/Accessory');
-
+exports.getAll = ()=> Accessory.find();
 exports.create = (accessoryData)=>Accessory.create(accessoryData);
